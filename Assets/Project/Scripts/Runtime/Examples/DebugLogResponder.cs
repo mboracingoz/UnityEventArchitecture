bd9db652401602c68bf1fd73a42951990e355e2c
Bora Cingöz <mboracingoz@gmail.com>
2026-03-10 15:42:34 +0300

    Added debug log responder example

---
+++ Assets/Project/Scripts/Runtime/Examples/DebugLogResponder.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventArchitecture.Examples
+{
+    public sealed class DebugLogResponder : MonoBehaviour
+    {
+        [SerializeField] private string _message = "Event received!";
+
+        public void LogMessa()
+        {
+            Debug.Log(_message, this);
+        }
+    }
+
+}
+
